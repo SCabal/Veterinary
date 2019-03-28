75e0a245bdf4d7d16bdf9a9e1ae6bae9bab9a168
Cabal <cabalsebas3@gmail.com>
2019-03-27 21:30:37 -0500

    Committed

---
+++ bin/ui/Main.java
@@ -0,0 +1,16 @@
+package ui;
+import model.*;
+import java.util.Scanner;
+public class Main
+{
+	private Veterinary veterinary;
+
+	public Main()
+	{
+		this.veterinary = veterinary;
+	}
+	public static void main(String[] args) 
+	{
+		Main main = new Main; 
+	}
+}
